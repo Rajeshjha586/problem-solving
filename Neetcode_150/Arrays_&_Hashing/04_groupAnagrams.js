90c29fcfbd6c33ac3425415c9b03ddf9a90ff5b1
Rajesh Jha <jharajesh035@gmail.com>
2026-01-31 23:55:20 +0530

    feat: Neetcode Problem 04 groupAnagrams

---
+++ Neetcode_150/Arrays_&_Hashing/04_groupAnagrams.js
@@ -0,0 +1,53 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+
+/***
+ * Hash Table
+ * Time Complexity: O(m * n)
+ * Space Complexity:
+ *       - O(m) extra space
+ *       - O(m * n) space for output list
+ *
+ * Where m is the number of strings and n is the length of the longest string.
+ */
+function groupAnagrams(strs) {
+  const res = {};
+  for (let s of strs) {
+    const count = new Array(26).fill(0);
+    for (let c of s) {
+      count[c.charCodeAt(0) - "a".charCodeAt(0)] += 1;
+    }
+
+    const key = count.join(",");
+    if (!res[key]) {
+      res[key] = [];
+    }
+    res[key].push(s);
+  }
+
+  return Object.values(res);
+}
+
+/***
+ * Sorting
+ * Time Complexity: O(m * nlogn)
+ * Space Complexity: O(m * n)
+ *
+ * Where m is the number of strings and n is the length of the longest string.
+ */
+function groupAnagrams(strs) {
+  const res = {};
+
+  for (let s of strs) {
+    const sortedS = s.split("").sort().join("");
+    if (!res[sortedS]) {
+      res[sortedS] = [];
+    }
+
+    res[sortedS].push(s);
+  }
+
+  return Object.values(res);
+}
